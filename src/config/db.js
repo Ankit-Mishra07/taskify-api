90b9797ea401543311963b6b5f1afbf2d485b22c
Ankit Mishra <ankit.mishra@idsnext.com>
2025-12-30 15:32:10 +0530

    db

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongodb+srv://AnkitMishra07:ankit07@cluster0.jxwud.mongodb.net
 const connectDB = async () => {
     mongoose.set('strictQuery', true);
-    await mongoose.connect(process.env.MONGO_URL + 'idsTaskify');
+    await mongoose.connect('mongodb+srv://AnkitMishra07:ankit07@cluster0.jxwud.mongodb.net/' + 'idsTaskify');
 }
 
 module.exports = connectDB;
